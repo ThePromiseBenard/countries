448bbd7c1acdb1c1c4d994561bb860c5b7c303a9
Promise Benard Augustine <promisebenard1@gmail.com>
2023-06-08 01:31:05 +0100

    chores: configured tailwind with the design systems

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="h-screen bg-slate-600 flex items-center justify-center text-4xl font-bold text-white capitalize">
+    <div className="flex items-center justify-center h-screen text-4xl font-bold text-white capitalize bg-lightBg dark:bg-darkBg">
       boy is grateful
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: '#2B3945',
+        darkBg: '#202C37',
+        darkText: '#FFFFFF',
+        lightText: '#111517',
+        lightInput: '#858585',
+        lightBg: '#FAFAFA',
+      },
+    },
   },
   plugins: [],
 };
